890b7401353e9a662d167545fcdcdbe4c567c6ab
danielemonaca <daniele.monaca1@gmail.com>
2022-11-05 02:53:11 +0100

    feat: adding common logic classes that will be needed

---
+++ lib/logic/common/rest_utils.dart
@@ -0,0 +1,13 @@
+/// Helpful when sending requests, as it will automatically format the url params
+class RestUtils {
+  static String encodeParams(Map<String, String> params) {
+    var s = '';
+    params.forEach((key, value) {
+      if (value.isNotEmpty && value != 'null') {
+        var urlEncode = Uri.encodeComponent(value);
+        s += '${s == '' ? '?' : '&'}$key=$urlEncode';
+      }
+    });
+    return s;
+  }
+}
