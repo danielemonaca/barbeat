6eae8541888ee57f3dd4c852db6b959fd04d949e
danielemonaca <daniele.monaca1@gmail.com>
2022-11-05 02:37:56 +0100

    chore: adding needed packages

--- lib/common_libs.dart
+++ lib/common_libs.dart
@@ -1,4 +1,20 @@
-/*
-* Here are all the libraries that are most 
-* common in every file, this is to avoid useless pollution
-*/
+export 'dart:math';
+
+export 'package:collection/collection.dart';
+export 'package:extra_alignments/extra_alignments.dart';
+export 'package:flextras/flextras.dart';
+export 'package:flutter/material.dart';
+export 'package:flutter/services.dart';
+export 'package:gap/gap.dart';
+export 'package:get_it/get_it.dart';
+export 'package:get_it_mixin/get_it_mixin.dart';
+export 'package:go_router/go_router.dart';
+export 'package:provider/provider.dart';
+export 'package:rnd/rnd.dart';
+export 'package:simple_rich_text/simple_rich_text.dart';
+export 'package:sized_context/sized_context.dart';
+export 'package:barbeat/assets.dart';
+export 'package:barbeat/main.dart';
+export 'package:barbeat/router.dart';
+export 'package:barbeat/styles/styles.dart';
+export 'package:flutter_animate/flutter_animate.dart';
