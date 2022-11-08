87badac1612b6abb2ad171e14c23b6680a047d24
Stefano <stef.pro@hotmail.com>
2022-11-08 17:11:11 -0500

    fix: can now pass in params into search, added clear icon

--- lib/ui/screens/searchByIngs/search_field.dart
+++ lib/ui/screens/searchByIngs/search_field.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
 
-class SearchField extends StatelessWidget {
-  SearchField({super.key});
+class SearchField extends StatefulWidget {
+  final String title;
+  final double width;
+  final Function() action;
 
-  final TextEditingController _controller = TextEditingController();
+  const SearchField(
+      {super.key,
+      required this.title,
+      required this.action,
+      required this.width});
 
   static const padding = EdgeInsets.symmetric(horizontal: 25, vertical: 20);
 
   @override
+  State<SearchField> createState() => _SearchFieldState();
+}
+
+class _SearchFieldState extends State<SearchField> {
+  final controller = TextEditingController();
+  String input = '';
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-      width: 300,
+      width: widget.width,
       decoration: const BoxDecoration(
         boxShadow: [
           BoxShadow(
@@ -21,24 +35,45 @@ class SearchField extends StatelessWidget {
         ],
       ),
       child: TextField(
-        controller: _controller,
-        onChanged: null, // temporary
+        controller: controller,
+        onChanged: (e) => {
+          setState(
+            () {
+              input = e;
+            },
+          ),
+          widget.action()
+        }, // temporary
         decoration: InputDecoration(
           fillColor: Colors.white,
           filled: true,
-          hintText: 'Ingredient Name',
+          hintText: widget.title,
           hintStyle: const TextStyle(
             fontSize: 17,
             color: Color(0xFFB5B5B5),
           ),
-          suffixIcon: const Align(
-            widthFactor: 3.5,
-            child: Icon(
-              Icons.search,
-              color: Color(0xFFB5B5B5),
-            ),
-          ),
-          contentPadding: padding,
+          suffixIcon: input.isEmpty
+              ? const Align(
+                  widthFactor: 3.5,
+                  child: Icon(
+                    Icons.search,
+                    color: Color(0xFFB5B5B5),
+                  ),
+                )
+              : Align(
+                  widthFactor: 1.5,
+                  child: IconButton(
+                    icon: const Icon(Icons.clear),
+                    color: const Color(0xFFB5B5B5),
+                    onPressed: () => setState(
+                      () {
+                        controller.clear();
+                        input = '';
+                      },
+                    ),
+                  ),
+                ),
+          contentPadding: SearchField.padding,
           border: OutlineInputBorder(
             borderSide: BorderSide.none,
             borderRadius: BorderRadius.circular(50),
