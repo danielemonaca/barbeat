67d88b54f86adcfb0a2ec35a996e4200c976d089
Stefano <stef.pro@hotmail.com>
2022-12-07 21:50:23 -0500

    fix: fixed functionality, things now working as intended

--- lib/ui/screens/searchByIngs/searchByIngs_main.dart
+++ lib/ui/screens/searchByIngs/searchByIngs_main.dart
@@ -20,26 +20,41 @@ class _SearchByIngredientsState extends State<SearchByIngredients> {
     const panelColor = Color(0xFFF1EFE8);
     Size size = MediaQuery.of(context).size;
 
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: backgroundColor,
-      body: SlidingUpPanel(
-        controller: panelController,
-        body: Stack(
-          children: [
-            const Background(),
-            NoIngsPic(size: size), // TODO: render conditionally***
-          ],
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: backgroundColor,
+        body: SlidingUpPanel(
+          onPanelClosed: () => FocusManager.instance.primaryFocus?.unfocus(),
+          controller: panelController,
+          body: Stack(
+            children: [
+              const Background(),
+              NoIngsPic(size: size), // TODO: render conditionally***
+            ],
+          ),
+          panelBuilder: (controller) => PanelWidget(
+            controller: controller,
+            panelController: panelController,
+          ),
+          color: panelColor,
+          borderRadius: const BorderRadius.vertical(top: Radius.circular(18)),
+          maxHeight: size.height * 0.77,
+          backdropEnabled: true,
         ),
-        panelBuilder: (controller) => PanelWidget(
-          controller: controller,
-          panelController: panelController,
-        ),
-        color: panelColor,
-        borderRadius: const BorderRadius.vertical(top: Radius.circular(18)),
-        maxHeight: size.height * 0.77,
-        backdropEnabled: true,
       ),
     );
+  }
+
+  // function to determine whether or not to return to home page
+  // based on if panel is open or not
+  Future<bool> _onWillPop() async {
+    if (panelController.isPanelOpen) {
+      panelController.close();
+      return false;
+    } else {
+      return true;
+    }
   }
 }
